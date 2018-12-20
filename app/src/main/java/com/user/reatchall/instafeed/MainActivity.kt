aa3d048bbb0fd5a9ee034ab8819efbbcba0c47af
sai charan reddy kantigari <charan15923@gmail.com>
2018-12-20 18:30:54 +0530

    fourth commit

--- app/src/main/java/com/user/reatchall/instafeed/MainActivity.kt
+++ app/src/main/java/com/user/reatchall/instafeed/MainActivity.kt
@@ -2,13 +2,13 @@ package com.user.reatchall.instafeed
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button
     }
 }
